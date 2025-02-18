53afd817656875470cee171c879b3b27ddef85e7
hieuhv203 <hieuhv203@gmail.com>
2025-02-18 16:43:59 +0700

    connect database

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://hieuhv203:08092003@localhost:27017/test-mongo', {
-
-    }),
+    MongooseModule.forRoot('mongodb://0.0.0.0:27017/crm', ),
     AuthModule, 
     UserModule, 
     BookmarkModule
